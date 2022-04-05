5c16239e681fd6037eb25509182d1d5cef403f79
Noufel17 <jn_naili@esi.dz>
2022-04-05 21:58:32 +0100

    completed intro

--- lib/pages/intro/intro.dart
+++ lib/pages/intro/intro.dart
@@ -58,21 +58,10 @@ class Intro extends StatelessWidget {
             ),
           ),
           PageViewModel(
-            titleWidget: Padding(
-              padding: const EdgeInsets.fromLTRB(0, 300.0, 0, 50.0),
-              child: Text(
-                'رمضان كريم',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontFamily: 'Kufam',
-                    fontSize: 32.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
-              ),
-            ),
+            title: 'رمضان كريم',
             body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
             footer: Padding(
-              padding: const EdgeInsets.fromLTRB(0, 50.0, 0, 0),
+              padding: const EdgeInsets.only(top: 50),
               child: ElevatedButton(
                 onPressed: () {},
                 style: ElevatedButton.styleFrom(
@@ -86,176 +75,138 @@ class Intro extends StatelessWidget {
                 ),
               ),
             ),
+            image: Image.asset(
+              'assets/intro3.png',
+              fit: BoxFit.cover,
+              height: double.infinity,
+              width: double.infinity,
+              alignment: Alignment.center,
+            ),
             decoration: PageDecoration(
+              titleTextStyle: TextStyle(
+                  fontFamily: 'Kufam',
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
               imagePadding: const EdgeInsets.all(0),
               contentMargin: const EdgeInsets.all(0),
               bodyAlignment: Alignment.center,
               bodyTextStyle: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontSize: 16.0,
-                  color: Colors.white),
-              boxDecoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/intro1.png'),
-                    fit: BoxFit.cover),
-              ),
-              imageAlignment: Alignment.center,
+                  fontFamily: 'Poppins', fontSize: 16.0, color: Colors.white),
+              fullScreen: true,
+              bodyFlex: 2,
+              imageFlex: 3,
             ),
           ),
           PageViewModel(
-            titleWidget: Padding(
-              padding: const EdgeInsets.fromLTRB(0, 300.0, 0, 50.0),
-              child: Text(
-                'رمضان كريم',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontFamily: 'Kufam',
-                    fontSize: 32.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
-              ),
-            ),
+            title: 'رمضان كريم',
             body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+            image: Image.asset(
+              'assets/intro2.png',
+              fit: BoxFit.cover,
+              height: double.infinity,
+              width: double.infinity,
+              alignment: Alignment.center,
+            ),
             footer: Padding(
-              padding: const EdgeInsets.fromLTRB(0, 50.0, 0, 0),
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  primary: HexColor('#C0954D').withOpacity(0.75),
-                  padding: const EdgeInsets.all(25),
-                  shape: const CircleBorder(),
-                ),
-                child: const Icon(
-                  Icons.arrow_forward_ios,
-                  size: 18,
-                ),
-              ),
-            ),
-            decoration: PageDecoration(
-              bodyAlignment: Alignment.center,
-              bodyTextStyle: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontSize: 16.0,
-                  color: Colors.white),
-              boxDecoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage('assets/intro3.png'),
-                      fit: BoxFit.cover)),
-            ),
-          ),
-          PageViewModel(
-            titleWidget: Padding(
-              padding: const EdgeInsets.fromLTRB(0, 300.0, 0, 50.0),
-              child: Text(
-                'رمضان كريم',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontFamily: 'Kufam',
-                    fontSize: 32.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
-              ),
-            ),
-            body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-            footer: Padding(
-              padding: const EdgeInsets.fromLTRB(0, 50.0, 0, 0),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(40),
-                          gradient: LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            colors: [
-                              HexColor('#D9B356').withOpacity(0.64),
-                              HexColor('#926526').withOpacity(0.64)
-                            ],
-                          ),
-                        ),
-                        height: 40,
-                        width: 133,
-                        child: const Center(
-                          child: Text(
-                            'Log In',
-                            style: TextStyle(
-                              fontFamily: 'Poppins',
-                              fontSize: 10,
-                              fontWeight: FontWeight.w700,
-                              color: Colors.white,
-                            ),
-                          ),
+              padding: const EdgeInsets.only(top: 50),
+              child:
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          HexColor('#D9B356').withOpacity(0.64),
+                          HexColor('#926526').withOpacity(0.64)
+                        ],
+                      ),
+                    ),
+                    height: 40,
+                    width: 133,
+                    child: const Center(
+                      child: Text(
+                        'Log In',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 10,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white,
                         ),
                       ),
                     ),
-                    SizedBox(width: 32.0),
-                    GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(40),
-                          gradient: LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            colors: [
-                              HexColor('#D9B356').withOpacity(0.64),
-                              HexColor('#926526').withOpacity(0.64)
-                            ],
-                          ),
-                        ),
-                        height: 40,
-                        width: 133,
-                        child: const Center(
-                          child: Text(
-                            'Sign Up',
-                            style: TextStyle(
-                              fontFamily: 'Poppins',
-                              fontSize: 10,
-                              fontWeight: FontWeight.w700,
-                              color: Colors.white,
-                            ),
-                          ),
+                  ),
+                ),
+                SizedBox(width: 32.0),
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          HexColor('#D9B356').withOpacity(0.64),
+                          HexColor('#926526').withOpacity(0.64)
+                        ],
+                      ),
+                    ),
+                    height: 40,
+                    width: 133,
+                    child: const Center(
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 10,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white,
                         ),
                       ),
                     ),
-                  ]),
+                  ),
+                ),
+              ]),
             ),
-
             decoration: PageDecoration(
+              titleTextStyle: TextStyle(
+                  fontFamily: 'Kufam',
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
               bodyAlignment: Alignment.center,
               bodyTextStyle: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontSize: 16.0,
-                  color: Colors.white),
-              boxDecoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage('assets/intro2.png'),
-                      fit: BoxFit.cover)),
+                  fontFamily: 'Poppins', fontSize: 16.0, color: Colors.white),
+              imagePadding: const EdgeInsets.all(0),
+              contentMargin: const EdgeInsets.all(0),
+              fullScreen: true,
+              bodyFlex: 2,
+              imageFlex: 3,
             ),
           ),
         ],
         onDone: () {},
         onSkip: () {},
-        showSkipButton: true,
+        showSkipButton: false,
         showDoneButton: false,
         showNextButton: false,
-        skip: const Text('Skip'),
-        skipColor: Colors.white,
         dotsDecorator: DotsDecorator(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(25.0),
-            ),
-            size: const Size(10.0, 8.0),
-            activeSize: const Size(24.0, 8.0),
-            activeColor: HexColor('#C0954D'),
-            color: HexColor('#E5E1E1'),
-            spacing: const EdgeInsets.symmetric(horizontal: 4.0),
-            activeShape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(25.0),
-            ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25.0),
+          ),
+          size: const Size(10.0, 5.0),
+          activeSize: const Size(22.0, 5.0),
+          activeColor: HexColor('#C0954D'),
+          color: HexColor('#E5E1E1'),
+          spacing: const EdgeInsets.symmetric(horizontal: 4.0),
+          activeShape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25.0),
+          ),
         ),
       ),
     );
